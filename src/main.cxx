1e6ee7bf92898e0811df89cdc3e1dc1d74f53044
Pierangelo Moraschi <moraschi.pierangelo@gmail.com>
2021-03-03 12:09:54 +0100

    Aggiunta richiesta del nome

--- src/main.cxx
+++ src/main.cxx
@@ -1,6 +1,10 @@
 #include <iostream>
 
-int main(){
-    std::cout <<"Hello, world!"<<std::endl;
+int main(int argc , char** argv){
+    if(argc != 2){
+        std::cout<<"Argomento richiesto: Nome"<<std::endl;
+        return -1;
+    }
+    std::cout <<"Hello, world! "<< argv[1] <<std::endl;
     return 0;
 }
